1e5ec0373ef3973379958f74dceba2a674fcb6e0
Lucas Garcia <garcia.ln@outlook.com>
2024-03-24 22:01:41 -0300

    kafka producer update

--- kafka/data_extraction.py
+++ kafka/data_extraction.py
@@ -2,6 +2,7 @@ import requests
 import polars as pl
 from confluent_kafka import Producer
 
+
 def fetch_data(url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -10,6 +11,7 @@ def fetch_data(url):
         print(f"Failed to fetch data from {url}")
         return None
 
+
 def create_dataframe(data):
     if data:
         df = pl.DataFrame(data)
@@ -17,16 +19,26 @@ def create_dataframe(data):
     else:
         return None
 
+
 def produce_message(producer, topic, message):
-    producer.produce(topic, message.encode('utf-8'))
+    producer.produce(topic, message.encode("utf-8"))
     producer.flush()
+
 
 def main():
     # Central Bank of Brazil API URLs
-    inflation_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/ultimos/30?formato=json"
-    job_rates_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.24369/ultimos/30?formato=json"
-    interest_rates_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/ultimos/30?formato=json"
-    public_spending_url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4426/ultimos/30?formato=json"
+    inflation_url = (
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/ultimos/30?formato=json"
+    )
+    job_rates_url = (
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.24369/ultimos/30?formato=json"
+    )
+    interest_rates_url = (
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/ultimos/30?formato=json"
+    )
+    public_spending_url = (
+        "https://api.bcb.gov.br/dados/serie/bcdata.sgs.4426/ultimos/30?formato=json"
+    )
 
     # Fetching data
     inflation_data = fetch_data(inflation_url)
@@ -41,12 +53,10 @@ def main():
     public_spending_df = create_dataframe(public_spending_data)
 
     # Kafka Producer
-    kafka_bootstrap_servers = 'localhost:9092'
-    kafka_topic = 'brazil_data'
+    kafka_bootstrap_servers = "localhost:9092"
+    kafka_topic = "brazil_data"
 
-    producer_conf = {
-        'bootstrap.servers': kafka_bootstrap_servers
-    }
+    producer_conf = {"bootstrap.servers": kafka_bootstrap_servers}
     producer = Producer(producer_conf)
 
     # Sending data to Kafka topic
@@ -58,6 +68,7 @@ def main():
         produce_message(producer, kafka_topic, interest_rates_df.to_json())
     if public_spending_df is not None:
         produce_message(producer, kafka_topic, public_spending_df.to_json())
+
 
 if __name__ == "__main__":
     main()
